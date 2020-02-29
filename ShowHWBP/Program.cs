bf2af6708dee76b9209388b80192c14411b5a7e7
dauthleikr <nino.neuw@gmail.com>
2020-02-29 11:41:46 +0100

    Project setup

---
+++ ShowHWBP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShowHWBP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
